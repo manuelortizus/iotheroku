7323c2d57706cc9bd7f3466834a2148b98dbbc67
manuelortizus <manuelfox2015@gmail.com>
2024-11-29 03:48:21 -0500

    Updating message of comment

--- esp-post.php
+++ esp-post.php
@@ -3,7 +3,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     echo "Datos crudos recibidos: \n";
     print_r(file_get_contents("php://input")); // Muestra todo el contenido del cuerpo
     echo "\nVariables POST: \n";
-    print_r($_POST); Muestra las variables procesadas
+    print_r($_POST); // Muestra las variables procesadas
 }
     else {
         http_response_code(405);
