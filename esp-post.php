fed71df3593317fcd59253100c38acfcb5707400
manuelortizus <manuelfox2015@gmail.com>
2024-11-29 03:45:55 -0500

    Going back to past versions v4 and printing  variables update

--- esp-post.php
+++ esp-post.php
@@ -1,4 +1,14 @@
 <?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    echo "Datos crudos recibidos: \n";
+    print_r(file_get_contents("php://input")); // Muestra todo el contenido del cuerpo
+    echo "\nVariables POST: \n";
+    print_r($_POST); Muestra las variables procesadas
+}
+    else {
+        http_response_code(405);
+        echo "Método no permitido";
+    }
     $user = $_POST['user'];
     $password = $_POST['pass'];
 
