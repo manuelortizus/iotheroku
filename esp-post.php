8bb429479f30e37bf30fdc888df95d3bfd1b5ef6
manuelortizus <manuelfox2015@gmail.com>
2024-11-29 03:29:16 -0500

    Going back to past versions that was working

--- esp-post.php
+++ esp-post.php
@@ -1,63 +1,6 @@
 <?php
-
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
-
-ini_set('display_errors' , 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-/*
-// Verifica si la solicitud es de tipo POST
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Obtiene los datos de los parametros 'user' y 'pass' del cuerpo del POST
-
     $user = $_POST['user'];
-    $pass = $_POST['pass'];
+    $password = $_POST['pass'];
 
-    // Envía la respuesta al cliente (ESP32)
-    echo "Hola $user tu clave es $pass";
-    echo "Hola";
-    echo $user;
-}
-else {
-    // Si no es un POST, devolver error 405 (Método no permitido)
-    header('HTTP/1.1 405 Method Not Allowed');
-    echo "Metodo no permitido";
-}
-
-*/
-
-
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $user = $_POST['user'];
-
-        echo "Datos recibidos: Usuario = $user";
-    }
-    else{
-        http_response_code(405); // Método no permitido
-        echo "Method not allowed"
-    }
-
-
-catch (Exception $e) {
-    http_response_code(500);
-    echo "Server error: " . $e->getMessage();
-}
-
-/*
-$user = $_POST['user'];
-$password = $_POST['pass'];
-
-echo "Hola".$user;
-echo "Hola $user";
-echo "Hola" + $user;
-echo "Mi nombres es";
-echo $user;
-
-
-?>
-*/
+    echo "hola ".$user." tu clave es ".$password;
 ?>
